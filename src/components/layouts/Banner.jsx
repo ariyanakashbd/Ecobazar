6d3441dcc214632702ba2eea93e36a787403c59c
Ariyan Akash <ariyanakash01303@gmail.com>
2026-06-02 10:46:58 +0600

    slider

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -4,7 +4,7 @@ import BannerBig from '../../assets/Bannar Big.webp';
 import BannerSmall from '../../assets/Bannar (1).webp';
 import BannerSmallTwo from '../../assets/Bannar.webp';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Navigation,Pagination,Scrollbar,Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
@@ -17,6 +17,13 @@ const Banner = () => {
         <div className='flex mt-6 mb-6 relative'>
            <div className='max-w-[872px]'>
             <Swiper
+            pagination={{
+              clickable:true,
+            }}
+            scrollbar={{
+              hide: true,
+            }}
+            autoplay={true}
             spaceBetween={0}
             slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
@@ -25,7 +32,7 @@ const Banner = () => {
                 prevEl: '.prev-arrow',
                 nextEl: '.next-arrow',
             }}
-            modules={[Navigation]}
+            modules={[Navigation,Pagination,Scrollbar,Autoplay]}
             >
             <SwiperSlide> <img src={BannerBig} alt="BannerBig" /></SwiperSlide>
             <SwiperSlide> <img src={BannerBig} alt="BannerBig" /></SwiperSlide>
@@ -37,7 +44,32 @@ const Banner = () => {
            
            </div>
            <div className='max-w-[423px] ml-6'>
-            <img className='mb-6' src={BannerSmallTwo} alt="BannerSmall" />
+            <Swiper
+            pagination={{
+              clickable:true,
+            }}
+            scrollbar={{
+              hide: true,
+            }}
+            autoplay={true}
+            spaceBetween={0}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            navigation={{
+                prevEl: '.prev-arrow',
+                nextEl: '.next-arrow',
+            }}
+            modules={[Navigation,Pagination,Scrollbar,Autoplay]}
+            >
+            <SwiperSlide> <img className='mb-6' src={BannerSmallTwo} alt="BannerSmall" /></SwiperSlide>
+            <SwiperSlide> <img className='mb-6' src={BannerSmallTwo} alt="BannerSmall" /></SwiperSlide>
+            <SwiperSlide> <img className='mb-6' src={BannerSmallTwo} alt="BannerSmall" /></SwiperSlide>
+            <SwiperSlide> <img className='mb-6' src={BannerSmallTwo} alt="BannerSmall" /></SwiperSlide>
+             <div className='prev-arrow'><FaArrowLeft /></div>
+            <div className='next-arrow'><FaArrowRight /></div>
+            </Swiper>
+            
             <img src={BannerSmall} alt="BannerSmall" />
            </div>
         </div>
